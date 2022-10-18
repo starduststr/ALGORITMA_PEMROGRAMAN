7627bd59494889f0a22ba70933af5b1b9eb2ba06
starduststr <starduststr0@gmail.com>
2022-10-18 11:07:43 +0700

    Update jobsheet 7

--- SEMESTER 1/JOBSHEET7/main.py
+++ SEMESTER 1/JOBSHEET7/main.py
@@ -1,11 +1,11 @@
 def pemanggil(x1=0, x2=0, x3=0):
-    nilai_prediksi = [10,9,8,9,2,9,3,4]
-    nilai_yang_diharapkan = [8,9,2,5,4,2,3,4,8]
+    nilai_prediksi = x1
+    nilai_yang_diharapkan = x2
 
     return nilai_prediksi, nilai_yang_diharapkan
 
 def backPropogation(y1=0, y2=0 ,y3=0):
-    nilai_prediksi, nilai_yang_diharapkan = pemanggil()
+    nilai_prediksi, nilai_yang_diharapkan = pemanggil([10,9,8,9,2,9,3,4], [10,9,8,9,2,9,3,4])
 
     hasil = []
 
