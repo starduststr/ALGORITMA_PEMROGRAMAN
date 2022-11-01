93d487faca19e450f566e4bb30cb2804488345ae
starduststr <starduststr0@gmail.com>
2022-11-01 09:05:04 +0700

    JOSBSHEET 8

---
+++ SEMESTER 1/JOBSHEET8/main.py
@@ -0,0 +1,26 @@
+def nilai(data):
+    nilai = float(data)
+    if nilai >= 80.1:
+        return "A"
+    elif nilai >= 75.1:
+        return "AB"
+    elif nilai >= 70.1:
+        return "B"
+    elif nilai >= 65.1:
+        return "BC"
+    elif nilai >= 60.1:
+        return "C"
+    elif nilai >= 55.1:
+        return "CD"
+    elif nilai >= 50.1:
+        return "D"
+    elif nilai < 50.1:
+        return "E"
+
+def main():
+    inputNilai = input("Masukan nilai : ")
+
+    print("NILAI ANDA ADALAH : ",nilai(inputNilai))
+
+
+main()
